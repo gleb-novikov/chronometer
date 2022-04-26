0fef6fed3d9258c6565124b89b2123f2db240f01
root <root@chronometer.local>
2022-04-26 12:55:58 +0000

    commit

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,4 +1,5 @@
 module.exports = [
+  'strapi::cors',
   'strapi::errors',
   {
     name: "strapi::security",
@@ -18,7 +19,6 @@ module.exports = [
       },
     },
   },
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
